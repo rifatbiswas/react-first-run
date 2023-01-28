0454f20d55e44485c5f7c727504fb67220058a81
Rifat Biswas <rifatbiswas2@gmail.com>
2023-01-28 09:27:06 -0800

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import Header from './component/Header';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='btn btn-primary'>Hello React</h1>
+      <h2>Bangladesh</h2>
+      <Header></Header>
+     <Person></Person>
+     <Person name="Rifat Biswas" job="Student 1"></Person>
+     <Person name="Rajebul Biswas" job="Student 2"></Person>
+     <Person name="Rifat Ahamed" job="Student 3"></Person>
+      
     </div>
   );
+}
+
+
+function Person(props){
+  return(
+    <div style={{color:'blue',}}>
+      {/* <h1>Mashrafi Mourtuza</h1> */}
+      <h1>{props.name}</h1>
+      <h2>Proffession: {props.job}</h2>
+    </div>
+  )
 }
 
 export default App;
---
+++ src/component/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Hader = () => {
+    return (
+        <div>
+            <h1 className='text-bg-dark m-5'>Hello frome ReactJs Header</h1>
+        </div>
+    );
+};
+
+export default Hader;
